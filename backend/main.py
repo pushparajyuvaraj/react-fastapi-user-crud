4347d0ec34e806f5095d1582175c7e97da6ab762
pushparajyuvaraj <pushparaj.yuvaraj@gmail.com>
2026-06-21 14:13:40 +0530

    Docket2

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes.user_routes import router as user_router
+
+app = FastAPI()
+
+origins=[
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI backend is running"}
+
+app.include_router(user_router)
+
